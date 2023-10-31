096e9f932b2f41f0ec179c4c13262569dbd74f31
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-10-31 12:59:01 +0700

    move application permission to domain layer

--- src/Domain/Authorization/ApplicationPermissions.cs
+++ src/Domain/Authorization/ApplicationPermissions.cs
@@ -1,9 +1,9 @@
 ﻿// Copyright (c) 2014-2024 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
 
-namespace Wangkanai.Architecture.Application.Authorization;
+namespace Wangkanai.Architecture.Authorization;
 
 [Flags]
-public enum Permissions
+public enum ApplicationPermissions
 {
 	None   = 0,
 	View   = 1,
