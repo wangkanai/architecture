4aa04dd3c96dbfc690517163c935b3aaed372979
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-10-31 16:36:50 +0700

    update identity configuration

--- src/Infrastructure/Persistence/Configurations/IdentityConfiguration.cs
+++ src/Infrastructure/Persistence/Configurations/IdentityConfiguration.cs
@@ -10,9 +10,35 @@ public class ApplicationUserConfiguration : IEntityTypeConfiguration<Application
 {
 	public void Configure(EntityTypeBuilder<ApplicationUser> builder)
 	{
+		builder.HasMany(user => user.UserClaims)
+		       .WithOne()
+		       .HasForeignKey(claim => claim.UserId)
+		       .IsRequired();
+
 		builder.HasMany(user => user.Logins)
 		       .WithOne()
 		       .HasForeignKey(login => login.UserId)
 		       .IsRequired();
+
+		builder.HasMany(user => user.Tokens)
+		       .WithOne()
+		       .HasForeignKey(token => token.UserId)
+		       .IsRequired();
+
+		builder.HasMany(user => user.UserRoles)
+		       .WithOne()
+		       .HasForeignKey(role => role.UserId)
+		       .IsRequired();
+	}
+}
+
+public class ApplicationUserTokenConfiguration : IEntityTypeConfiguration<ApplicationUserToken>
+{
+	public void Configure(EntityTypeBuilder<ApplicationUserToken> builder)
+	{
+		builder.HasOne(token => token.User)
+		       .WithMany(user => user.Tokens)
+		       .HasForeignKey(token => token.UserId)
+		       .OnDelete(DeleteBehavior.Cascade);
 	}
 }
