fa08bb158db621c827aeb892d2d6a523b094dc26
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-10-31 19:07:07 +0700

    add domain model configuration

---
+++ src/Infrastructure/Persistence/Configurations/BrandConfiguration.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) 2014-2024 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using Wangkanai.Architecture.Models;
+
+namespace Wangkanai.Architecture.Infrastructure.Persistence.Configurations;
+
+public class BrandConfiguration : IEntityTypeConfiguration<Brand>
+{
+	public void Configure(EntityTypeBuilder<Brand> builder)
+	{
+		builder.Property(brand => brand.Name)
+		       .HasMaxLength(50)
+		       .IsRequired();
+	}
+}
