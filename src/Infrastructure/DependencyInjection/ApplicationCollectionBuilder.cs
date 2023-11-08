a32182727fdf85a06ed690e0e50adeb945154fe0
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-11-08 11:14:13 +0700

    resolve license header

--- src/Infrastructure/DependencyInjection/ApplicationCollectionBuilder.cs
+++ src/Infrastructure/DependencyInjection/ApplicationCollectionBuilder.cs
@@ -1,7 +1,6 @@
-﻿// Copyright (c) 2014-2024 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+﻿// Copyright (c) 2014-2024 Sarin Na Wangkanai, All Rights Reserved.
 
 // ReSharper disable once CheckNamespace
-
 namespace Microsoft.Extensions.DependencyInjection;
 
 public static class ApplicationCollectionBuilder
