ebebb1f065d2d4ae4582e5d20c36958a2f9b204d
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-11-10 15:58:41 +0700

    migration blazor and publish first package template

--- pack.ps1
+++ pack.ps1
@@ -1,9 +1,14 @@
-remove-item -path .\packed\*.*
+remove-item -path .\signed\*.*
 remove-item -path .\artifacts\*.*
-new-item -Path packed -ItemType Directory -Force
 
-nuget pack .\Architecture.nuspec -OutputDirectory .\artifacts
+new-item -Path artifacts -ItemType Directory -Force
+new-item -Path signed    -ItemType Directory -Force
 
-dotnet nuget sign .\artifacts\*.nupkg -v diag --timestamper http://timestamp.digicert.com --certificate-subject-name "Sarin Na Wangkanai" -o .\packed
+# nuget pack .\Architecture.nuspec -OutputDirectory .\artifacts
+dotnet pack Wangkanai.Architecture.Templates.csproj -c Release -o .\artifacts
+dotnet nuget sign .\artifacts\*.nupkg -v diag --timestamper http://timestamp.digicert.com --certificate-subject-name "Sarin Na Wangkanai" -o .\signed
+dotnet nuget push .\signed\*.nupkg -k $env:NUGET_API_KEY  -s https://api.nuget.org/v3/index.json --skip-duplicate
 
-dotnet nuget push .\packed\*.nupkg -k $env:NUGET_API_KEY  -s https://api.nuget.org/v3/index.json --skip-duplicate
+remove-item -path .\bin -recurse -force
+remove-item -path .\obj -recurse -force
+remove-item -path .\artifacts\*.*
