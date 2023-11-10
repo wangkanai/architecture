f2e4c26b5d6acd056c0cb9bf9dead44aac4a529f
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-11-10 16:38:02 +0700

    add maui mobile solution template structure

---
+++ content/Maui/src/Mobile/Platforms/Android/MainApplication.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) 2014-2024 Sarin Na Wangkanai, All Rights Reserved.
+
+using Android.App;
+using Android.Runtime;
+
+namespace Wangkanai.Architecture
+{
+	[Application]
+	public class MainApplication : MauiApplication
+	{
+		public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+			: base(handle, ownership)
+		{
+		}
+
+		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	}
+}
---
+++ content/Maui/src/Mobile/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) 2014-2024 Sarin Na Wangkanai, All Rights Reserved.
+
+using Foundation;
+
+namespace Wangkanai.Architecture
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate
+	{
+		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	}
+}
