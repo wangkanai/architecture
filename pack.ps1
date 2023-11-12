f5097c264ffea45fcf4e4a0fdf37a3179f1f8964
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2023-11-12 11:53:27 +0700

    update dotnet new template configuration
    The propose is the make the template for discoverable using the `dotnet new search clean`

--- pack.ps1
+++ pack.ps1
@@ -1,16 +1,14 @@
-remove-item -path .\signed\*.*
-remove-item -path .\artifacts\*.*
+remove-item -path .\signed\*.*    -Force
+remove-item -path .\artifacts\*.* -Force
 
-new-item -Path artifacts -ItemType Directory -Force
-new-item -Path signed    -ItemType Directory -Force
+new-item -Path artifacts -ItemType Directory -Force | out-null
+new-item -Path signed    -ItemType Directory -Force | out-null
 
-# nuget pack .\Architecture.nuspec -OutputDirectory .\artifacts
-dotnet pack Wangkanai.Architecture.Templates.csproj -c Release -o .\artifacts
+dotnet build  Wangkanai.Architecture.Templates.csproj -c Release
+dotnet pack   Wangkanai.Architecture.Templates.csproj -c Release -o .\artifacts
 dotnet nuget sign .\artifacts\*.nupkg -v diag --timestamper http://timestamp.digicert.com --certificate-subject-name "Sarin Na Wangkanai" -o .\signed
 dotnet nuget push .\signed\*.nupkg -k $env:NUGET_API_KEY  -s https://api.nuget.org/v3/index.json --skip-duplicate
 
-remove-item -path .\bin -recurse -force
-remove-item -path .\obj -recurse -force
 remove-item -path .\artifacts\*.*
 
-version.ps1
+.\version.ps1
